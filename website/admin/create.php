41aa93c460cebe95ff23bbda282aeb88c4564109
Felix Winterleitner <39999795+seltat@users.noreply.github.com>
2020-09-22 13:19:08 +0200

    fixed a bug introduced by adding the no_of_trees feature that prevented new trees from being created

--- website/admin/create.php
+++ website/admin/create.php
@@ -2,6 +2,7 @@
 require '../php/database.php';
 
 session_start();
+
 if (!isset($_SESSION["username"])) {
     http_response_code(401);
     die();
@@ -11,14 +12,19 @@ if(!isset($_POST)) {
     http_response_code(400);
     die();
 }
+
+#region RETRIEVE post body
 try {
     $tree = json_decode(file_get_contents('php://input'));
+    /*
+     * file_get_contents('php://input') does not work when request is redirected, e.g. from http to https
+     * see https://stackoverflow.com/questions/19146984/file-get-contentsphp-input-always-returns-an-empty-string for more details
+     */
 }
 catch (Exception $ex) {
     http_response_code(400);
     die("Invalid Format");
 }
-
 $desc = $tree -> description;
 $no_trees = $tree -> no_of_trees;
 $date_planted = date("Y-m-d", strtotime($tree -> date_planted));
@@ -28,20 +34,18 @@ $y = $tree -> y;
 
 $images = $tree -> images;
 $sponsors = $tree -> sponsors;
-
+#endregion
 
 $conn = getDbConnection();
 
-
-
-
-$stmt = $conn->prepare("INSERT INTO trees (description, no_of_trees, date_planted, location, location_name) VALUE (?, ?, POINT(?,?), ?)");
-$stmt->bind_param("ssdds", $desc, $no_trees, $date_planted, $x, $y, $location);
+// Create Tree in DB
+$stmt = $conn->prepare("INSERT INTO trees (description, no_of_trees, date_planted, location, location_name) VALUE (?, ?, ?, POINT(?,?), ?)");
+$stmt->bind_param("sisdds", $desc, $no_trees, $date_planted, $x, $y, $location);
 $stmt->execute();
 $tree_id = $stmt->insert_id;
 
 
-#region Assign Images
+#region Assign Images to Tree
 $image_id = null;
 $text = null;
 
@@ -55,12 +59,12 @@ foreach ($images as $image) {
 }
 #endregion
 
-#region Create Sponsors
+#region Create Sponsors in DB
 
 $name = null;
 $contr = null;
 
-$stmt = $conn->prepare("INSERT INTO sponsors (tree_id, name, contribution) VALUE (?,?,?)");
+$stmt = $conn->prepare("INSERT INTO sponsors (tree_id, `name`, contribution) VALUE (?,?,?)");
 $stmt->bind_param("iss", $tree_id, $name, $contr);
 
 foreach ($sponsors as $sponsor) {
@@ -68,8 +72,8 @@ foreach ($sponsors as $sponsor) {
     $contr = $sponsor -> contribution;
     $stmt->execute();
 }
-
 #endregion
+
 http_response_code(200);
 echo $tree_id;
 ?>
